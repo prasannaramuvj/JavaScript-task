d31740b5b7a6ff6b28cb703f8c5dc27e6ed5cdac
prasannaramuvj <prasannaramu2004@gmail.com>
2025-08-26 16:55:23 +0530

    add crud

---
+++ Crud-operation/script.js
@@ -0,0 +1,17 @@
+let form = document.getElementById("myforms");
+let imageInput = document.querySelector(".img");
+let file = document.getElementById("inputImage");
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let city =document.getElementById("city");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let post = document.getElementById("post");
+let date = document.getElementById("date");
+let userInfo = document.getElementById("data");
+let submitbtn = document.querySelector(".submit");
+
+
+
+
+
