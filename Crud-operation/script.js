5d6af7a70ffcbc96cf22415fa5f2075c8e720b52
prasannaramuvj <prasannaramu2004@gmail.com>
2025-08-29 15:58:36 +0530

    updated

--- Crud-operation/script.js
+++ Crud-operation/script.js
@@ -1,7 +1,7 @@
 
 let form = document.getElementById("myForm");
-// let imgInput = document.querySelector(".img");
-// let file = document.getElementById("imgInput");
+let imgInput = document.querySelector(".img");
+let file = document.getElementById("imgInput");
 let userName = document.getElementById("name");
 let age = document.getElementById("age");
 let city =document.getElementById("city");
@@ -14,7 +14,6 @@ let userInfo = document.getElementById("data");
 let modal = document.getElementById('useForm');
 let modalTitle = document.querySelector("#useForm .modal-title");
 let newUserBtn = document.querySelector(".newUser");
-
 
 
 let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')):[]
@@ -33,16 +32,46 @@ newUserBtn.addEventListener('click',()=>{
   submitBtn.innerText = "submit"
   modalTitle.innerText = "Fill the Form"
   isEdit = false
+  // imgInput.src ="./image/Profile Icon.webp"
+
   form.reset()
+
+
+ document.querySelectorAll('.error').forEach(spans=> spans.innerHTML = "");
+
+ document.querySelectorAll('.line').forEach(lines=>lines.style.border="");
+  imgInput.src = "img/Profile Icon.webp";
+
 })
 
 
+
+file.onchange=function(){
+
+  if(file.files[0].size<1000000){
+    let fileReader = new FileReader();
+
+    fileReader.onload=function(e){
+      imgUrl = e.target.result;
+      imgInput.src=imgUrl
+    }
+
+    fileReader.readAsDataURL(file.files[0])
+  }
+  else{
+    alert('file size is high');
+  }
+}
+
+
 function showInfo(){
+
 
  userInfo.innerHTML = ""
   getData.forEach((element,index) =>{
     let CreateElement = `<tr class = "employeeDetails">
     <td>${index+1}</td>
+    <td><img src = "${element.picture}" width="50" height="50"></td>
     <td>${element.employeeName}</td>
     <td>${element.employeeAge}</td>
     <td>${element.employeeCity}</td>
@@ -52,11 +81,11 @@ function showInfo(){
     <td>${element.startDate}</td>
 
     <td>
-    <button class="btn btn-success" onclick="readInfo('${element.employeeName}','${element.employeeAge}','${element.employeeCity}','${element.employeeEmail}','${element.employeePhone}','${element.employeePost}','${element.startDate}')" data-bs-toggle="modal" data-bs-target="#readData">
+    <button class="btn btn-success" onclick="readInfo('${element.picture}','${element.employeeName}','${element.employeeAge}','${element.employeeCity}','${element.employeeEmail}','${element.employeePhone}','${element.employeePost}','${element.startDate}')" data-bs-toggle="modal" data-bs-target="#readData">
     <i class="bi bi-eye"></i>
     </button>
 
-    <button class="btn btn-primary" onclick="editInfo(${index},'${element.employeeName}','${element.employeeAge}','${element.employeeCity}','${element.employeeEmail}','${element.employeePhone}','${element.employeePost}','${element.startDate}')" data-bs-toggle="modal" data-bs-target="#useForm">
+    <button class="btn btn-primary" onclick="editInfo(${index},'${element.picture}','${element.employeeName}','${element.employeeAge}','${element.employeeCity}','${element.employeeEmail}','${element.employeePhone}','${element.employeePost}','${element.startDate}')" data-bs-toggle="modal" data-bs-target="#useForm">
     <i class="bi bi-pencil-square"></i>
     </button>
 
@@ -75,7 +104,8 @@ function showInfo(){
 
   showInfo()
 
-function readInfo(name, age, city, email, phone, post, Sdate){
+function readInfo( pic,name, age, city, email, phone, post, Sdate){
+    document.querySelector('#showImg').src = pic;
     document.querySelector('#showName').value = name;
     document.querySelector("#showAge").value = age;
     document.querySelector("#showCity").value = city;
@@ -86,11 +116,11 @@ function readInfo(name, age, city, email, phone, post, Sdate){
 
 }
 
-function editInfo(index, name, Age, City, Email, Phone, Post, Sdate){
+function editInfo(index,pic, name, Age, City, Email, Phone, Post, Sdate){
     isEdit = true            
     editId = index           
-    // imgInput.src = pic       
-    userName.value = name    
+    imgInput.src = pic       
+    userName.value = name 
     age.value = Age
     city.value = City
     email.value = Email
@@ -115,9 +145,13 @@ function deleteInfo(index){
 
 form.addEventListener('submit' ,(e)=>{
   e.preventDefault()
+   
 
+let isFormvalid = checkValidation(userName)
 
+if(isFormvalid){
   const information = {
+    picture:imgInput.src == undefined ? "img/Profile Icon.webp" :imgInput.src,
     employeeName:userName.value,
     employeeAge:age.value,
     employeeCity:city.value,
@@ -145,5 +179,137 @@ form.addEventListener('submit' ,(e)=>{
     form.reset()
 
 
+      imgInput.src = "img/Profile Icon.webp";
+
+
+
+
+}
+
+  // imgInput.src = "./image/Profile Icon.webp"
 
 })
+
+
+
+function checkValidation(userName){
+  
+let isValid = true;
+
+  if(userName.value.trim() === "" || /\d/.test(userName.value.trim())){
+    isValid = false;
+    userName.style.border = "1px solid red";
+    document.querySelector('#nameerrorid').innerHTML= "Name is required";
+    
+  
+  }
+  else{
+    document.querySelector('#nameerrorid').innerHTML= "";
+        userName.style.border = "";
+
+  }
+//-------------------------------------------------------------------------------------------//
+  
+  if(age.value.trim() === ""){
+    isValid = false;
+    age.style.border = "1px solid red";
+    document.querySelector('#ageerrorid').innerHTML = "Age is required";
+  }
+  else if(age.value.trim()<18){
+    document.querySelector('#ageerrorid').innerHTML = " Age must be above 18";
+    isValid = false;
+    age.style.border = "1px solid red";
+
+  }
+  else{
+    document.querySelector('#ageerrorid').innerHTML = "";
+    age.style.border = "";
+
+
+  }
+//-----------------------------------------------------------------------------------------------//
+
+ if(city.value.trim() === "" || /\d/.test(city.value.trim())){
+    isValid = false;
+    city.style.border = "1px solid red";
+    document.querySelector('#cityerrorid').innerHTML = "City is required";
+  }
+  else{
+    document.querySelector('#cityerrorid').innerHTML = "";
+    city.style.border = "";
+
+
+  }
+//-----------------------------------------------------------------------------------------//
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ if(email.value.trim() === ""){
+    isValid = false;
+    email.style.border = "1px solid red";
+
+    document.querySelector('#emailerrorid').innerHTML = "Email is required";
+  }
+
+  else if(!emailPattern.test(email.value.trim())){
+    isValid = false;
+    email.style.border = "1px solid red";
+
+    document.querySelector('#emailerrorid').innerHTML = " Enter the valid format of Email id";
+
+
+  }
+  else{
+    email.style.border = "";
+
+    document.querySelector('#emailerrorid').innerHTML = "";
+
+  }
+
+  //--------------------------------------------------------------------------------//
+    const phonePattern = /^\d{10}$/;
+
+   if(phone.value.trim() === "" ||!phonePattern.test(phone.value.trim())){
+    isValid = false;
+    phone.style.border = "1px solid red";
+
+    document.querySelector('#phoneerrorid').innerHTML = "phone no is required number must be in 10 digit";
+  }
+  else{
+    document.querySelector('#phoneerrorid').innerHTML = "";
+    phone.style.border = "";
+
+
+  }
+  //-----------------------------------------------------------------------------------//
+    const postPattern = /^\d{6}$/;
+
+   if(post.value.trim() === "" || !postPattern.test(post.value.trim())){
+    isValid = false;
+    post.style.border = "1px solid red";
+
+    document.querySelector('#posterrorid').innerHTML = "post is required number must be in six digit";
+  }
+  else{
+    document.querySelector('#posterrorid').innerHTML = "";
+    post.style.border = "";
+
+
+  }
+
+  //----------------------------------------------------------------------------------------//
+   if(sDate.value.trim() === ""){
+    isValid = false;
+    sDate.style.border = "1px solid red";
+
+    document.querySelector('#sDateerrorid').innerHTML = "starting date  is required";
+  }
+  else{
+    document.querySelector('#sDateerrorid').innerHTML = "";
+    sDate.style.border = "";
+
+
+  }
+
+//---------------------------------------------------------//
+  return isValid
+}
